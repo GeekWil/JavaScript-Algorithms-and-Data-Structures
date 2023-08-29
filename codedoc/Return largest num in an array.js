7b499c62c58c29b5c2b943fd44359cb55528b9d8
Wilson <110122926+GeekWil@users.noreply.github.com>
2023-08-29 08:25:03 -0700

    Create Return largest num in an array.js

---
+++ codedoc/Return largest num in an array.js
@@ -0,0 +1,23 @@
+function largestOfFour(arr) {
+
+  let max = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let subArray = arr[i];
+    let largestNumber = subArray[0];
+
+    for (let j = 1; j < subArray.length; j++) {
+      if (subArray[j] > largestNumber) {
+        largestNumber = subArray[j];
+      }
+    }
+
+    max.push(largestNumber);
+  }
+
+  return max;
+}
+
+
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
